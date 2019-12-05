381bd2eec28c5103b856f807029a34368ef263c1
Charlotte Capitanchik <CharlotteAnne@users.noreply.github.com>
2019-12-05 14:01:23 +0000

    Generalise for command line input & tidy

--- imaps/sandbox/clusterCoverage/clusterCoverageOverSpecificSamples.sh
+++ imaps/sandbox/clusterCoverage/clusterCoverageOverSpecificSamples.sh
@@ -5,27 +5,21 @@
 # Then format for DeSeq
 
 # INPUT
-files="PHO92_Sc_4TUiCLIP_IME4del_SPO_REP1_G.peaks.bed,PHO92_Sc_4TUiCLIP_IME4del_SPO_REP2_G.peaks.bed,PHO92_Sc_4TUiCLIP_IME4del_SPO_REP3_G.peaks.bed,PHO92_Sc_4TUiCLIP_IME4del_SPO_REP4_G.peaks.bed,PHO92_Sc_4TUiCLIP_WT_SPO_REP1_G.peaks.bed,PHO92_Sc_4TUiCLIP_WT_SPO_REP2_G.peaks.bed,PHO92_Sc_4TUiCLIP_WT_SPO_REP3_G.peaks.bed,PHO92_Sc_4TUiCLIP_WT_SPO_REP4_G.peaks.bed,WT_Sc_4TUiCLIP_WT-xl365_SPO_REP1_G.peaks.bed,PHO92_Sc_4TUiCLIP_IME4del-6Jxl_SPO_REP1_G.peaks.bed"
-stem="PHO92_iCLIP"
-cluster_dist="20"
-folder="."
-
+files=$1 # peak files "file1,file2,file3"
+xlink_files=$2 # xlink files in same order "file1,file2,file3"
+stem=$3 # "m6a_miclip"
+cluster_dist=$4 # 10
+folder=$5 # for output eg. "../u2os_omiclip/cluster_analysis"
 
 # MAIN
-# Create a new directory to store all these results and a file containing what files were used to generate them
-#mkdir ${folder}
-#chmod -R 755 ${folder}
-# Convert to a list we can iterate over
-file_list=$(echo ${files} | tr "," "\n")
 # Make a read me that contains the files we are using
 echo ${file_list} > ${folder}/README.txt
 # Convert to a line separated by spaces instead of commas
 file_list_space=$(echo ${files} | sed s'/,/ /g')
+file_xlsites=$(echo ${xlink_files} | sed s'/,/ /g')
 # Make a merged megafile of peaks
 echo ${file_list_space}
 cat ${file_list_space} > ${folder}/${stem}_merged_peaks.bed
-# Prepare crosslink sites
-file_xlsites=$(echo ${file_list_space} | sed s'/\.peaks.bed/\.Aligned\.out\.sorted\.cdnacounts/g' | sed s'/m6A_Sc/\.\.\/\.\.\/results\/genome_xlsites\/PHO92_Sc/g')
 # Make a merged megafile of crosslinks
 cat ${file_xlsites} > ${folder}/${stem}_merged_xlsites.bed
 
@@ -44,7 +38,7 @@ cat ${folder}/${stem}_${cluster_dist}_nearestTo.bed ${folder}/${stem}_${cluster_
 # For each crosslink file get the coverage over the joint clusters
 for file in $file_xlsites; do
 	echo $file
-	filename=$(echo ${file} | sed s'/\.\.\/\.\.\/results\/genome_xlsites\///g' | sed s'/\.Aligned\.out\.sorted\.cdnacounts//g')
+	filename=$(basename ${file})
 	echo $filename
 	bedtools sort -i ${file} > ${filename}.sorted
 	bedtools map -a ${folder}/${stem}_${cluster_dist}_merged_3_clusters.bed -b ${filename}.sorted -c 5 -o sum -s > ${folder}/${filename}.mergedClusterCoverage.${cluster_dist}.bed
